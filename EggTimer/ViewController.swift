3864bf76eddb28fe4966a1a4561b762b659e9c2f
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-04-07 15:28:50 -0700

    Added timer function to button click

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,24 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+    let eggTime : [String: Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
     
-
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        var secondsRemaining = self.eggTime[hardness]!
+        print(eggTime[hardness]!)
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+               if secondsRemaining > 0 {
+                   print ("\(secondsRemaining) seconds")
+                   secondsRemaining -= 1
+               } else {
+                   Timer.invalidate()
+               }
+           }
+    }
+    
 }
