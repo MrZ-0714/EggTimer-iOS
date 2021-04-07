40f8218d1d58ab4878ff0cbbcb92788e024c3d8b
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-04-07 16:31:33 -0700

    Added progress bar to show the cooking progress. Changed title label to reflect what hardness is being prepared.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -13,13 +13,17 @@ class ViewController: UIViewController {
     let eggTime : [String: Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
     var timer = Timer()
     var secondsRemaining = 60
+    var hardness = ""
+    
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         timer.invalidate()
-        
-        let hardness = sender.currentTitle!
+        hardness = sender.currentTitle!
         secondsRemaining = self.eggTime[hardness]!
+        titleLabel.text = "Cookign an \(hardness) egg!"
+        
         print(eggTime[hardness]!)
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
@@ -27,12 +31,16 @@ class ViewController: UIViewController {
         }
     
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
+        if secondsRemaining >= 0 {
             print("\(secondsRemaining) seconds remaining")
             secondsRemaining -= 1
+            
+            progressBar.progress = 1 - Float((Float(secondsRemaining) / Float(self.eggTime[hardness]!)))
+
         };
-        if secondsRemaining == 0 {
-            titleLabel.text = "DONE!"
+        if secondsRemaining < 0 {
+            timer.invalidate()
+            titleLabel.text = "EGG IS DONE!"
         }
     }
     
